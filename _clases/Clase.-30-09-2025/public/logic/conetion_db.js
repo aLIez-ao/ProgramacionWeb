a880222b412153c6ec54cd98dacd30a073a8f3f8
aLIez <molvi113@gmail.com>
2025-10-22 17:46:45 -0600

    Estructura del proyecto. Primer maquetado

--- _clases/Clase.-30-09-2025/public/logic/conetion_db.js
+++
@@ -1,8 +0,0 @@
-// variables que alamacenan la configuración de la conexión
-const host = "localhost:3307";
-const user = "root";
-const password = "1212";
-const database = "crud_app";
-
-// exportar la configuración de la conexión
-module.exports = { host, user, password, database };
--- _clases/README.md
+++
@@ -1,2 +0,0 @@
-# ProgramacionWeb
-Repositorio que contiene proyectos, ejercicios y prácticas realizadas durante el curso de Programación Web.
