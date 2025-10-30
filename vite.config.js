497e84001b8a404eef461b8b2d6dfb4d9b3d84f9
aLIez <molvi113@gmail.com>
2025-10-30 01:41:40 -0600

    Primer despliegue de la pagina web

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,9 @@ export default defineConfig({
     port: 3000,
     open: true,
   },
+  build: {
+    outDir: "../dist",
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
